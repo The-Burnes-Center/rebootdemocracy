fe3ad7c499beb815da29d3c1cca3c860691e22b0
Anirudh Dinesh <ad3572@nyu.edu>
2023-07-07 16:58:05 +0530

    events

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from '../pages/index.vue';
-
+import Events from '../pages/events.vue';
 
 const routes = [
   { path:"/", component: Home },
+  { path:"/events", component: Events },
 ]
 
 const router = createRouter({
