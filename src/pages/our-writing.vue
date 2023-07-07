521aa8fb597dc4a550dc4cce7e175809f283fc0c
Anirudh Dinesh <ad3572@nyu.edu>
2023-07-07 21:28:52 +0530

    our-writing-page

--- src/pages/our-writing.vue
+++ src/pages/our-writing.vue
@@ -29,14 +29,8 @@ export default {
   data() {
     return {
                                  
+      articleData: [],
       indexData: [],
-      eventsData:[],
-      InnovateUSData:[],
-      selectedStatus: undefined,
-      eventTitle: "",
-      eventDescription: "",
-      seriesData:[],
-      alleventsData: [],
       directus: new Directus('https://directus9.thegovlab.com/'),
       path:this.$route.fullPath,
     }
@@ -45,10 +39,7 @@ export default {
   created() {
 
     this.indexData = this.directus.items("reboot_democracy");
-    this.fetchIndex();
-    this.fetchEvents();
-    this.fetchInnovateUS();
-    this.fetchSeries();  
+    this.fetchIndex(); 
     },
 
 //  mounted()
@@ -89,43 +80,6 @@ export default {
         return isFuture(new Date(d1));
       },
 
-      formatSeriesData: async function formatSeriesData() {
-
-         self = this;
-
-        if(self.selectedStatus == undefined) {
-          self.eventTitle = "";
-          self.eventDescription = "";
-          this.alleventsData = this.eventsData.concat(this.InnovateUSData);
-          this.alleventsData.sort((a, b) => a.event_element.date - b.event_element.date);
-          
-        }
-        else if (self.selectedStatus != undefined){
-          self.eventTitle = self.selectedStatus.title;
-          self.eventDescription = self.selectedStatus.description;
-
-          if(self.selectedStatus.title == "InnovateUS Workshops"){
-              
-            //  this.InnovateUSData_elements = this.InnovateUSData.map(obj => obj.event_element);
-            this.alleventsData = this.InnovateUSData;
-            this.alleventsData.sort((a, b) => a.event_element.date - b.event_element.date);
-          }
-
-          else if(self.selectedStatus.title != "InnovateUS Workshops"){
-          // this.eventsData_elements = this.eventsData.map(obj => obj.event_element);
-          let tempData = this.eventsData.filter(function (e) {
-          return e.event_element.event_series[0].general_events_series_id.title == self.selectedStatus.title
-        });
-          this.alleventsData = tempData;
-          this.alleventsData.sort((a, b) => a.event_element.date - b.event_element.date);
-          }
-        }
-
-        
-      },
-    forceChange() {
-    this.selectedStatus = "Reboot Democracy Lecture Series";
-  },
     fetchIndex: function fetchIndex() {
       self = this;
 
@@ -143,48 +97,14 @@ export default {
       self.indexData =  item.data;
       });
     },
-        fetchSeries: function fetchSeries() {
-      self = this;
 
-      this.directus
-      .items('general_events_series')
-      .readByQuery({
-         meta: 'total_count',
-         limit: -1,
-         fields: [
-          '*.*'
-       ],
-       
-      })
-      .then((item) => {
-      self.seriesData =  item.data;
 
-      });
-    },
-        fetchInnovateUS: function fetchInnovateUS() {
-      self = this;
 
-      this.directus
-      .items('innovate_us_workshops')
-      .readByQuery({
 
-         meta: 'total_count',
-         limit: -1,
-         sort:["-id"],
-         fields: [
-          '*.*',"instructor.innovate_us_instructors_id.*","instructor.innovate_us_instructors_id.headshot.*",
-       ],
-       
-      })
-      .then((item) => {
 
-      let TempInnovateUSData =  item.data;
-      let temp = TempInnovateUSData.map(element => ({ event_element: element, series_name: "InnovateUS Workshop" }));
-       self.InnovateUSData = temp;
-this.formatSeriesData();
-      });
 
-    },
+
+
 
     fetchEvents: function fetchEvents() {
       self = this;
@@ -193,23 +113,30 @@ this.formatSeriesData();
       .items('reboot_democracy_resources')
       .readByQuery({
           filter: {
-          type: 
-          { 
-            _eq: "Event"
-           }
+                $or: [
+            {
+              type: {
+                _eq: "Event"
+              }
+            },
+            {
+              type: {
+                _eq: "Article"
+              }
+            }
+            ]
+
           },
          meta: 'total_count',
          limit: -1,
          sort:["-id"],
          fields: [
-          '*.*','thumbnail.*','event_series.general_events_series_id.*'
+          '*.*'
        ],
        
       })
       .then((item) => {
-      self.eventsData =  item.data;
-      self.eventsData  = self.eventsData.map(element => ({ event_element: element, series_name: element.event_series[0].general_events_series_id.title }));
-     this.formatSeriesData();
+      self.articleData =  item.data;
       });
     }
 
@@ -221,66 +148,8 @@ this.formatSeriesData();
 <!-- Header Component -->
 <header-comp></header-comp>
 
-<div class="events-hero">
-  <div class="events-row">
-    <h1>Events</h1>
-    <div class="custom-select">
-      <select v-model="selectedStatus" @change="formatSeriesData()">
-        <option :value="undefined">All Events</option>
-        <option v-for="series in seriesData" :value="series">{{series.title}}</option>
-      </select>
-      <i class="fa-solid fa-angle-down"></i>
-    </div>
-  </div>
-   <div class="events-row">
-    <div  v-if="eventTitle != ''" class="event-information">
-      <h3 class="eyebrow" >{{eventTitle}}</h3>
-      <h1>{{eventDescription}}</h1>
-    </div>
-  </div>
-</div>
 
-<div class="event-grid-section">
-  <h3>Upcoming Events</h3>
-  <div class="event-grid-row">
-    <div class="event-grid-col"  v-for="event_item in alleventsData" v-show="FutureDate(new Date(event_item.event_element.date))">
-      <div class="event-grid-item">
-        <div class="event-image">
-            <img v-if="!event_item.event_element.instructor && event_item.event_element.thumbnail" :src="this.directus._url + 'assets/' + event_item.event_element.thumbnail.id">
-            <div v-for="(instructor_item,index) in event_item.event_element.instructor" v-show="index < 1"> 
-                <img :src="this.directus._url + 'assets/' + instructor_item.innovate_us_instructors_id.headshot.id">
-            </div>
 
-           
-        </div>
-        <div class="event-text">
-          <h5 class="eyebrow">{{event_item.series_name}}</h5>
-          <h2>{{event_item.event_element.title}}</h2>
-          <p> {{ formatDateTime(new Date(event_item.event_element.date)) }} ET </p>
-
-          <p v-html="event_item.event_element.description"></p>
-          <a class="btn btn-primary btn-dark btn-medium">Register</a>
-      </div>
-    </div>
-    </div>
-  </div>
-</div>
-
-<div class="event-grid-section">
-  <h3>Past Events</h3>
-  <div class="past-event-grid-row">
-    <div class="past-event-grid-item"  v-for="event_item in alleventsData" v-show="PastDate(new Date(event_item.event_element.date))">
-      <div class="past-event-col-1">
-          <h5 class="eyebrow">{{event_item.series_name}}</h5>
-          <h2>{{event_item.event_element.title}}</h2>
-      </div>
-      <div class="past-event-col-2">
-         <p v-html="event_item.event_element.description"></p>
-         <a class="btn btn-secondary btn-dark btn-medium">Watch</a>
-      </div>
-    </div>
-  </div>
-</div>
 <mailing-list-comp></mailing-list-comp>
 <footer-comp></footer-comp>
 
