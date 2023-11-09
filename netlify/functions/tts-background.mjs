f246c34d42204617dfe56c48468630bae171e1d8
Stephan <schmid91@mail.com>
2023-11-09 12:05:08 -0800

    rd process

--- netlify/functions/tts-background.mjs
+++ netlify/functions/tts-background.mjs
@@ -107,8 +107,57 @@ export default async (req, context) => {
               
               // Function to upload speech (placeholder, replace with your actual implementation)
               async function generateAndUploadSpeech(text) {
-                console.log('Uploading speech with text:', text);
-                // Your generateAndUploadSpeech implementation here...
+                const response = await openai.audio.speech.create({
+                  model: "tts-1-hd",
+                  voice: "shimmer",
+                  input: text,
+                });
+              
+                // Check the response is OK
+                if (response.status !== 200) {
+                  throw new Error('Failed to generate speech from OpenAI.');
+                }
+              
+                // Collect stream data into a buffer
+                const chunks = [];
+                for await (const chunk of response.body) {
+                  chunks.push(chunk instanceof Buffer ? chunk : Buffer.from(chunk));
+                }
+                const buffer = Buffer.concat(chunks);
+              
+                // Create form-data instance
+                const form = new FormData();
+                form.append('file', buffer, {
+                  filename: bodyres.collection+'_'+bodyres.id+'.mp3', // The filename
+                  contentType: 'audio/mpeg', // The MIME type of the audio
+                  knownLength: buffer.length // Optional, necessary for some setups to calculate Content-Length
+                });
+              
+                const directusFileEndpoint =DIRECTUS_URL+'/files';
+              
+                // Prepare the request headers with the Bearer token
+                const headers = {
+                  'Authorization': 'Bearer '+DIRECTUS_AUTH_TOKEN, // replace with an actual token
+                };
+              
+                // Merge the headers from form-data with Directus token
+                const finalHeaders = { ...form.getHeaders(), ...headers };
+              
+                // Make the request to Directus to upload the file
+                const fileResponse = await fetch(directusFileEndpoint, {
+                    method: 'POST',
+                    body: form,
+                    headers: finalHeaders
+                });
+              
+                if (!fileResponse.ok) {
+                    const errorBody = await fileResponse.text();
+                    throw new Error(`Error uploading file: ${errorBody}`);
+                }
+              
+                const directusResponse = await fileResponse.json();
+              
+                return directusResponse;
               }
               
               // Main function to run the process
@@ -131,8 +180,6 @@ export default async (req, context) => {
               // Start the process
               runProcess();
 
-              // Call the function with your desired text
-              // generateAndUploadSpeech(bodyres.content).catch(console.error);
 
               /////////////
               
