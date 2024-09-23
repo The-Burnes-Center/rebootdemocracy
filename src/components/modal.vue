df00dbb99d4639a75709801dfa3757bfce769c14
ayushkhanvilkar10 <khanvilkar.a@northeastern.edu>
2024-09-23 16:00:31 -0400

    Optimized Modal component

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -1,59 +1,32 @@
 <script>
-
-import { Directus } from '@directus/sdk';
-
 export default {
-  data() {
-    return {
-      modalData: [],
-      directus: new Directus('https://content.thegovlab.com/'),
+  props: {
+    modalData: {
+      type: Object,
+      required: true
     }
-  },
-  created() {
-    this.fetchModal();
   },
   methods: {
     closeFunc() {
       this.$emit('close');
     },
-    btnFunct(button_url)
-    {
-
-      localStorage.setItem("Reboot Democracy","off");
-      window.open(button_url,'_self')
-    },
-    fetchModal: function fetchModal() {
-    
-      this.directus
-      .items('reboot_democracy_modal')
-      .readByQuery({
-         meta: 'total_count',
-         limit: -1,
-         fields: [
-          '*.*'
-       ]
-      })
-      .then((item) => {
-      this.modalData =  item.data;
-      });
+    btnFunct(button_url) {
+      localStorage.setItem("Reboot Democracy", "off");
+      window.open(button_url, '_self');
     }
-
   }
 }
 </script>
+
 <template>
-<div class="modal-bg">
-  <div class="modal-section">
-    <!-- <span @click="closeFunc" class="modal-close">close</span> -->
-    <i @click="closeFunc" class="fa-regular fa-circle-xmark modal-close"></i>
-    <div class="row">
-      
-        <h3>{{modalData.title}}</h3>
-        <div class="modal-content" v-html = "modalData.content"></div>
-         <a @click="btnFunct(modalData.button_url)" :href="modalData.button_url" class="btn btn-primary btn-medium">{{modalData.button_text}}</a>
-
-
+  <div class="modal-bg">
+    <div class="modal-section">
+      <i @click="closeFunc" class="fa-regular fa-circle-xmark modal-close"></i>
+      <div class="row">
+        <h3>{{ modalData.title }}</h3>
+        <div class="modal-content" v-html="modalData.content"></div>
+        <a @click="btnFunct(modalData.button_url)" :href="modalData.button_url" class="btn btn-primary btn-medium">{{ modalData.button_text }}</a>
+      </div>
     </div>
-  </div>
   </div>
 </template>
