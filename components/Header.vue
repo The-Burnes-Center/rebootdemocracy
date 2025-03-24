b33f737975361070c5921d1549402bd2c39f505b
Raveena Ingale <ingale.r@northeastern.edu>
2025-03-24 16:15:34 -0400

    fix:nuxt

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="bg-gray-800 text-white p-4">
-    <h1 class="text-xl font-semibold">My Nuxt App - Header component</h1>
+    <h1 class="text-xl font-semibold">Nuxt Vue Blog app- Header component</h1>
   </header>
 </template>
 
 <script setup>
-// optional logic here
+
 </script>
