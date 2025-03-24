bdec555884baf31b0249b5dd33d84a726bb34000
Raveena Ingale <ingale.r@northeastern.edu>
2025-03-24 15:58:04 -0400

    Directus plugin installation (#34)
    * added-directus-plugin
    
    * fix:directus and basic setup

---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="bg-gray-800 text-white p-4">
+    <h1 class="text-xl font-semibold">My Nuxt App - Header component</h1>
+  </header>
+</template>
+
+<script setup>
+// optional logic here
+</script>
