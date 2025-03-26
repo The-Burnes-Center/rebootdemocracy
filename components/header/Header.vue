1c863a3e130b25f48facc0fcbce3c6197cfb77f0
Raveena Ingale <ingale.r@northeastern.edu>
2025-03-26 12:36:12 -0400

    Css setup font colors (#38)
    * fix:layout
    
    * fix:component auto import
    
    * configured font and images

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <header class="header-section">
     <h1>Nuxt Vue Blog app- Header component</h1>
+    <p>This is the header component</p>
   </header>
   <MyButton />
   <MyButton>Custom Label</MyButton>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <section class="p-6">
     <h1 class="text-3xl font-bold mb-4">Welcome to Reboot blog app</h1>
     <p class="text-lg text-gray-600">This is the homepage powered by Nuxt 3.</p>
+    <img src="/images/exampleImage.png"  class="mt-4" />
   </section>
 </template>
 
