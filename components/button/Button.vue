1a29874da6ab3a45b082406690e46c4f50440bc5
Raveena Ingale <ingale.r@northeastern.edu>
2025-03-28 17:14:23 -0400

    button component done (#39)

---
+++ components/button/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    @click="onClick"
+    class="base__button"
+    :aria-label="ariaLabel || label"
+    :style="{
+      '--bg-color': bgColor,
+      '--border-color': borderColor || bgColor,
+      '--bg-opacity': opacity,
+      '--text-color': textColor,
+    }"
+  >
+    <span class="base__btn-slot">
+      <slot>{{ label }}</slot>
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+interface ButtonProps {
+  onClick: () => void;
+  label: string;
+  bgColor: string;
+  textColor: string;
+  borderColor: string;
+  opacity: number;
+  ariaLabel?:string;
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  opacity: 1,
+  borderColor: ''
+});
+</script>
--- components/header/Header.vue
+++ components/header/Header.vue
@@ -2,9 +2,16 @@
   <header class="header-section">
     <h1>Nuxt Vue Blog app- Header component</h1>
     <p>This is the header component</p>
+   
+     <Button
+      label="Learn more"
+      bgColor="#FAFAFA"
+      textColor="#0d63eb"
+      borderColor="#0d63eb"
+      :opacity="0.2"
+      :onClick="handleClick"
+    />
   </header>
-  <MyButton />
-  <MyButton>Custom Label</MyButton>
 </template>
 
 <script setup></script>
