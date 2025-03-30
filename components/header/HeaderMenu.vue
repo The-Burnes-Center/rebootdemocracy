3d96a43ba141bd3f409696f808059cca81e23bd1
Raveena Ingale <ingale.r@northeastern.edu>
2025-03-29 20:53:52 -0400

    header-desktop fix

---
+++ components/header/HeaderMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="menu__section">
+    <NuxtLink
+      v-for="(item, index) in items"
+      :key="index"
+      :to="item.to"
+      class="header-menu__item"
+      :target="item.external ? '_blank' : 'self'"
+    >
+     <span class="header-menu__label">{{ item.label }}</span>
+    </NuxtLink>
+  </section>
+</template>
+
+<script lang="ts" setup>
+interface MenuItem {
+  label: string;
+  name: string;
+  url?: string;
+  external?: boolean;
+}
+
+defineProps<{
+  items: MenuItem[];
+  initialTab?: number;
+}>();
+</script>
