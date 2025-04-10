fd8c369d1d8e5486c914ce7d78c8eb531a5d9575
Stephan <schmid91@mail.com>
2025-04-10 14:43:34 -0700

    images DO netlify transformator - rollup 2

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,31 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePluginRadar } from "vite-plugin-radar";
 import ViteFonts from 'vite-plugin-fonts'
-import { format } from 'date-fns';
-import { isPast } from 'date-fns';
-
 import Pages from "vite-plugin-pages";
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   assetsInclude: ['**/*.png'],
-  data () {
-    return {
-      format,
-      isPast
-    }
-  },
   plugins: [
     VitePluginRadar({
-      // Google Analytics tag inject
       enableDev: true,
       analytics: {
         id: 'G-L78LX2HS2N',
       },
     }),
     vue(),
-        rollupOptions: {
-      external: [
-        /^\/\.netlify\/images/, // Mark Netlify image URLs as external
-      ],
-    },
     Pages({
       dirs: 'src/pages',
       extensions: ['vue', 'ts'],
     }),
     ViteFonts({
       typekit: {
-          /**
-           * Typekit project id
-           */
-          id: 'tde3xym',
-
-          /**
-           * enable non-blocking renderer
-           *   <link rel="preload" href="xxx" as="style" onload="this.rel='stylesheet'">
-           * default: true
-           */
-          defer: true
-        },
+        id: 'tde3xym',
+        defer: true
+      },
     })
   ],
   build: {
@@ -57,16 +33,9 @@ export default defineConfig({
       external: [
         /^\/\.netlify\/images/, // Mark Netlify image URLs as external
       ],
-    },
+    }
   },
   server: {
     host: '0.0.0.0',
-    // hmr: {
-    //   host: 'localhost', // you could make this an ENV var
-    //   port: '3005',
-    //   path: '/'
-    // }
   },
-}
-
-)
+});
