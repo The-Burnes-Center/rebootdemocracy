dfef04314ff0e6fbdf4fb7ed50ae595e964bcd4b
Raveena Ingale <ingale.r@northeastern.edu>
2025-04-15 12:31:03 -0400

    Image fix algolia (#115)
    * fix:blog-page search
    
    * global-search:original
    
    * fix:search
    
    * lloadblog-refactor
    
    * tab-change handled
    
    * added:filtering for blogs
    
    * fix:weight
    
    * netlify-toml added
    
    * fix:netlify-config
    
    * fix:dependency
    
    * fix:client
    
    * fix:gitignore and toml
    
    * fix:publish
    
    * fix:toml
    
    * fix:publish
    
    * fix:reverted to dist
    
    * fix:in progress netlify
    
    * fix:toml
    
    * fix:netlify toml in progress
    
    * fix:netlify.toml
    
    * fix:our-work
    
    * fix:netlify
    
    * inprogres
    
    * fix:added generae
    
    * fix:netlify
    
    * added '/'
    
    * fix: change build path for genertae
    
    * reveert
    
    * fix:image-blog
    
    * fix:home-page index image s3

--- pages/index.vue
+++ pages/index.vue
@@ -210,11 +210,24 @@ const tabOptions = computed(() => [
 ]);
 
 // Methods
-function getImageUrl(image: any, width: number = 512): string {
-  return image?.filename_disk 
-    ? `${DIRECTUS_URL}/assets/${image.filename_disk}?width=${width}`
-    : "/images/exampleImage.png";
+function getImageUrl(
+  image: { filename_disk?: string } | null | undefined,
+  width: number = 512
+): string {
+  if (!image?.filename_disk) {
+    return "/images/exampleImage.png"; 
+  }
+
+  const s3BaseUrl =
+    "https://thegovlab-files.nyc3.cdn.digitaloceanspaces.com/thegovlab-directus9/uploads/";
+  const fullS3Url = s3BaseUrl + image.filename_disk;
+
+  // Use Netlify Image CDN in production only
+  return process.env.NODE_ENV === "production"
+    ? `/.netlify/images?url=${encodeURIComponent(fullS3Url)}&w=${width}`
+    : fullS3Url;
 }
+
 
 const getAuthorName = (post: BlogPost): string => {
   const author = post.authors?.[0]?.team_id;
