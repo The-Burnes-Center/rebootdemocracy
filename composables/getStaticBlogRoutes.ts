2811c41229763a341ff75830d3615529bd5a9a60
Raveena Ingale <ingale.r@northeastern.edu>
2025-04-29 13:29:50 -0400

    static html files prerender (#170)

---
+++ composables/getStaticBlogRoutes.ts
@@ -0,0 +1,19 @@
+// scripts/fetchStaticRoutes.ts
+import { createDirectus, rest, readItems } from '@directus/sdk';
+
+export const getStaticBlogRoutes = async (): Promise<string[]> => {
+  const directus = createDirectus('https://content.thegovlab.com').with(rest());
+
+  const posts = await directus.request(
+    readItems('reboot_democracy_blog', {
+      fields: ['slug'],
+      filter: {
+        status: { _eq: 'published' },
+        date: { _lte: '$NOW(-5 hours)' },
+      },
+      limit: -1,
+    })
+  );
+
+  return posts.map((post: any) => `/blog/${post.slug}`);
+};
