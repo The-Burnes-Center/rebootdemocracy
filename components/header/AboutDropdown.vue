e3e1707053cbf721fedeebdf9ac4e4adf72cf629
Raveena Ingale <ingale.r@northeastern.edu>
2025-04-29 14:56:51 -0400

    fix:dropdown-about

---
+++ components/header/AboutDropdown.vue
@@ -0,0 +1,34 @@
+<!-- AboutDropdown.vue -->
+<template>
+  <div class="about-dropdown__container" v-if="isOpen">
+    <div class="about-dropdown__content">
+      <NuxtLink 
+        v-for="(item, index) in items" 
+        :key="index" 
+        :to="item.to" 
+        class="about-dropdown__item"
+        @click="emitItemClick(item, $event)"
+      >
+        <span class="about-dropdown__item-label">{{ item.label }}</span>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { DropdownItem, MenuItem } from "@/types/index.ts";
+
+interface Props {
+  items: DropdownItem[];
+  isOpen: boolean;
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits(["close", "item-click"]);
+
+function emitItemClick(item: MenuItem, event: MouseEvent): void {
+  emit('item-click', item, event);
+  emit('close');
+}
+</script>
+
