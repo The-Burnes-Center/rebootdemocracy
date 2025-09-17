2d0648a23c752207a3b4c233001cc38ea2683e98
Raveena Ingale <ingale.r@northeastern.edu>
2025-09-17 14:45:41 -0400

    add:updates

---
+++ src/helpers/weeklyNewsHelper.js
@@ -0,0 +1,28 @@
+export function fetchPostData(slug) {
+  
+  return this.directus
+    .items("reboot_democracy_weekly_news")
+    .readByQuery({
+      meta: "total_count",
+      limit: -1,
+      fields: ["*.*",
+              "image.*",
+              "items.reboot_democracy_weekly_news_items_id.*",
+              "items.reboot_democracy_weekly_news_items_id.related_links.*",
+              "items.reboot_democracy_weekly_news_items_id.related_links.reboot_weekly_news_related_news_id.*"],
+      filter: {
+        _and: [
+          {
+            edition: {
+              _eq: slug,
+            },
+          },
+          {
+            status: {
+              _eq: "published",
+            },
+          }
+        ],
+      },
+    });
+}
