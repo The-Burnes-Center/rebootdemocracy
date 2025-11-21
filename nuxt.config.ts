5029c40e62db6949319a4427c0facdbdb1e328c0
Stephan <stephan@Blues-iMac-5.local>
2025-11-20 16:23:20 -0800

    try out isr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,9 @@ import { getStaticNewsRoutes } from './composables/getStaticNewsRoutes';
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
   ssr: true,
   
